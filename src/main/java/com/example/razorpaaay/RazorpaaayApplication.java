0817f38cd6bd13d2be2ba3444da6a5b1d15e3ca1
abhijrathod <nueonboy@gmail.com>
2025-03-16 20:18:18 +0530

    Initial commit

---
+++ src/main/java/com/example/razorpaaay/RazorpaaayApplication.java
@@ -0,0 +1,13 @@
+package com.example.razorpaaay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorpaaayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RazorpaaayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/razorpaaay/RazorpaaayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.razorpaaay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazorpaaayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
